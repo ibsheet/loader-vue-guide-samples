3c2909f6014e53c37f38957212c48de5834396be
이재호 <jhlee@ibleaders.co.kr>
2021-10-25 16:08:01 +0900

    add vuex 4 example

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+// vuex 4 선언하는 구간. 예시 적용
+import { createStore } from 'vuex';
+
+export const CREATE_SHEET = 'CREATE_SHEET';
+export const CREATE_SAMPLE = 'CREATE_SAMPLE';
+export const REMOVE_SAMPLE = 'REMOVE_SAMPLE';
+
+const initalState = {
+    title: ''
+};
+
+export default createStore({
+    modules: initalState
+});
