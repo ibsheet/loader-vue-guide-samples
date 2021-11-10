9d51e992d7d586bc4549ff770f3aaf1ead41dc61
박동진 <djpark@ibleaders.co.kr>
2021-11-10 18:35:28 +0900

    store mount

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import Router from './router';
 import App from './App.vue';
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
+import store from './store'
 
-const app = createApp(App).use(Quasar, quasarUserOptions);
+const app = createApp(App).use(Quasar, quasarUserOptions).use(store);
 
 app.use(Router);
 app.mount('#app');
