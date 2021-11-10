1e57475f3a61630a89a3bd8410e3b54fb029ab88
박동진 <djpark@ibleaders.co.kr>
2021-11-10 18:35:15 +0900

    loader config 설정

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,23 @@
 
 <script>
 import Layout from './components/layout/Layout';
+import loader from '@ibsheet/loader'
 
 export default {
   name: 'App',
   components: {
     Layout
-  }
+  },
+  mounted() {
+    loader.config({
+      registry: [
+        {
+          name: 'ibsheet',
+          baseUrl: '/ibsheet'
+        }
+      ]
+    });
+  },
 }
 </script>
 <style>
