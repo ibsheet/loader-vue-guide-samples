ac40701123d1d6e8274ed7f75c214ee9e54a0eb7
박동진 <djpark@ibleaders.co.kr>
2021-11-10 18:35:03 +0900

    About removesheet 호출
    해당 부분은 삭제 될 수 있음...

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <h2>About</h2>
+    <div id='sheetContainer'></div>
   </div>
 </template>
 
 <script>
+// import loader from '@ibsheet/loader'
+import { computed } from "vue";
+import { useStore } from "vuex";
+// let SHEET_ID = ''
+
 export default {
+
   name: 'About',
   components: {
+  },
+  setup() {
+    const store = useStore();
+    const sheetId = computed(() => store.state.Sheet.sheet_id);
+    const reomoveSheet = () => store.commit("REMOVE_SAMPLE");
+    return { reomoveSheet, sheetId }
   }
 }
 </script>
