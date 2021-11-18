addcea4caf50bcc2a48f83093d2f2f9bb5d76fb9
박동진 <djpark@ibleaders.co.kr>
2021-11-18 19:07:59 +0900

    첫 화면 디자인 변경중

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,37 @@
 <template>
-    <div>
-        <h2>안녕하세요. 첫화면 Home 컴포넌트입니다.</h2>
-    </div>
+  <div class="q-pa-md row items-start q-gutter-md">
+
+    <q-card class="my-card" v-for="img in image" :key="img">
+      <div><img :src = img> </div>
+      <q-card-actions align="center">
+        <q-btn flat round to="/Text"> TextType </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <!-- <q-card class="my-card">
+      <div><img src = "../assets/subsum.png"> </div>
+      <q-card-actions align="center">
+        <q-btn flat round to> SubSum </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card">
+      <div><img src = "../assets/tree.png"> </div>
+      <q-card-actions align="center">
+        <q-btn flat round to> Tree </q-btn>
+      </q-card-actions>
+    </q-card> -->
+
+  </div>
 </template>
 <script>
 export default {
+ data() {
+     return {
+        image: [
+            require('../assets/type.png'), require('../assets/subsum.png')
+        ]
+     }
+ },
 }
 </script>
