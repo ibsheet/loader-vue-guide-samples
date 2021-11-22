5179191d298fd51eef70755152b9da0a737cb27a
박동진 <djpark@ibleaders.co.kr>
2021-11-22 18:39:43 +0900

    여러개시트 생성
    이건 좀 아닌거같고 나중에 다른 방법을 찾아봐야할 것 같음. mutation 내에서 하는게 좋지않을까?

--- src/views/Sheet.vue
+++ src/views/Sheet.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <div>{{pageName}} 페이지 입니다</div>
-    <button v-if="pageName == 'SubSum'">dd</button>
+    
+    <div class="text-h6">{{pageName}}</div>
+    <!-- <button v-if="pageName == 'SubSum'">dd</button> -->
     <div id='sheetContainer'></div>
+    <div v-if="pageName == 'Multiple'" id='sheetContainer2'></div>
+    <div v-if="pageName == 'Multiple'" id='sheetContainer3'></div>
   </div>
 </template>
 
 <script>
 import { computed } from "vue";
 import { useStore } from "vuex";
-// import { SheetSampleData } from '../shared/ibsheet-data'
-// const ibsample_data  = SheetSampleData
+// import { setData } from '../samples/dataload'
 
 
 
@@ -19,25 +21,34 @@ export default {
     const store = useStore();
     const pageName = computed(() => store.state.Page.name);
     const sheetId = computed(() => store.state.Sheet.sheet_id);
-    const sheetInfo = computed(() => store.state.Sheet.options);
-    const sheetData = computed(() => store.state.Sheet.sheet_data);
     const test = computed(() => store.getters);
     // 시트 생성&삭제
     const createSheet = (sampleData) => store.commit('CREATE_SHEET', sampleData);
+    const changeSheetDiv = (divId) => store.commit('CHANGE_SHEETDIV', divId);
     const reomoveSheet = (id) => store.commit("REMOVE_SAMPLE", id);
 
-    return { pageName, createSheet, reomoveSheet, test, sheetInfo, sheetData, sheetId};
+    const dataLoad = (cnt) => store.commit("CHANGE_DATA", cnt);
+
+    
+
+    return { pageName, createSheet, reomoveSheet, test, sheetId, dataLoad, changeSheetDiv};
   },
   mounted() {
-    this.createSheet(this.test);
-    // url 로 바로 접근하는 경우에 시트 생성을 위한구간. 방법이 이게 맞는지는 의문
-    // switch(this.pageName) {
-    //   case "SubSum" : 
-    //     break;
-    //   case "TextType" : 
-    //     this.createSheet(this.test);
-    //     break;
-    // }
+    if (this.$store.state.Page.name == "DataLoad") {
+      this.changeSheetDiv('sheetContainer')
+      this.dataLoad(200);
+      this.createSheet(this.test);
+    } else if (this.$store.state.Page.name == "Multiple") {
+      this.changeSheetDiv('sheetContainer')
+      this.createSheet(this.test);
+      this.changeSheetDiv('sheetContainer2')
+      this.createSheet(this.test);
+      this.changeSheetDiv('sheetContainer3')
+      this.createSheet(this.test);
+    } else {
+      this.changeSheetDiv('sheetContainer')
+      this.createSheet(this.test);
+    }
   },
 
 }
